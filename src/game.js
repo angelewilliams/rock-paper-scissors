a26a602c0dfd8be641fa52799591b2c1d1f3397c
Angele Williams <angele.williams5@gmail.com>
2022-03-03 22:48:02 -0700

    Test checkGame method in Game class

--- src/game.js
+++ src/game.js
@@ -1,50 +1,34 @@
 class Game {
   constructor(){
-    this.player1 = new Player('Computer', '🙍');
+    this.player1 = new Player('Human', '🙍');
     this.player2 = new Player('Computer', '💻' );
     this.gameType = null;
     //classic or difficult
     this.gameData = [this.player1.fighter];
     this.winConditionsClassic = ['rock > scissors', 'paper > rock', 'scissors > paper']
-    this.winConditionsChallenge = ['rock > scissors', 'paper > rock', 'scissors > paper']
-    this.winner = this.checkGame();
+    this.winConditionsChallenge = ['change this to be for chef challenge']
+    this.winner = null;
   }
 
   autoplay(){
-    this.player1.makeRandomChoice(this.classic);
-    this.player2.makeRandomChoice(this.classic);
+    this.player1.makeRandomChoice(this.player1.classic);
+    console.log(this.player1.fighter);
+    this.player2.makeRandomChoice(this.player2.classic);
+    console.log(this.player2.fighter);
   }
 
-  // checkGameClassic(){
-  //   if (this.player1.fighter === this.player2.fighter ){
-  //     return "It's a draw!"
-  //   }
-  //   else if(this.player1.fighter === 'rock' && this.player2.fighter === 'scissors'){
-  //     this.player1.wins++;
-  //     return `${this.player1.name} won!`
-  //   }
-  //   else if(this.player1.fighter === 'paper' && this.player2.fighter === 'rock'){
-  //     this.player1.wins++;
-  //     return `${this.player1.name} won!`
-  //   }
-  //   else if(this.player1.fighter === 'scissors' && this.player2.fighter === 'paper'){
-  //     this.player1.wins++;
-  //     return `${this.player1.name} won!`
-  //   }
-  //   this.player2.wins++;
-  //   return `${this.player1.name} won!`
-  //
-  // }
   checkGameExperiment(){
-    if (this.winConditionsClassic.includes(`${this.player1.fighter} > ${this.player2.fighter}`){
+    if (this.winConditionsClassic.includes(`${this.player1.fighter} > ${this.player2.fighter}`)) {
       this.winner = this.player1.name;
       return `${this.player1.name} won!`
-    }
+      }
     else if (this.player1.fighter === this.player2.fighter){
       return "It's a draw!"
+      }
+    else {
+      this.player2.wins++;
+      return `${this.player2.name} won!`
     }
-    this.player2.wins++;
-    return `${this.player1.name} won!`
   }
   recordWin() {
       if (this.player1.winner) {
@@ -67,3 +51,24 @@ class Game {
 
 
 // object within an array within an object
+
+//RAINY DAY or OH SHOOT CODE
+  // checkGameClassic(){
+  //   if (this.player1.fighter === this.player2.fighter ){
+  //     return "It's a draw!"
+  //   }
+  //   else if(this.player1.fighter === 'rock' && this.player2.fighter === 'scissors'){
+  //     this.player1.wins++;
+  //     return `${this.player1.name} won!`
+  //   }
+  //   else if(this.player1.fighter === 'paper' && this.player2.fighter === 'rock'){
+  //     this.player1.wins++;
+  //     return `${this.player1.name} won!`
+  //   }
+  //   else if(this.player1.fighter === 'scissors' && this.player2.fighter === 'paper'){
+  //     this.player1.wins++;
+  //     return `${this.player1.name} won!`
+  //   }
+  //   this.player2.wins++;
+  //   return `${this.player1.name} won!`
+  // }
